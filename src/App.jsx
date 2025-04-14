68c5a5964327e841b1d6b5dde9d1c795070e50e7
maheep05 <maheepsingh458@gmail.com>
2025-04-14 22:33:54 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,38 @@ function App() {
   return (
     <BrowserRouter>
       <div>
+      <div className="text-center text-sm bg-black text-white p-1 flex flex-row  justify-center items-center gap-2 ">
+        <p>Made with ❤️ by Maheep Singh Saluja</p>
+
+        <a
+          href="https://github.com/Maheep05"
+          target="_blank"
+          className="hover:text-black transition"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className="w-5 h-5"
+          >
+            <path d="M12 0a12 12 0 00-3.79 23.4c.6.11.82-.26.82-.58v-2.16c-3.34.73-4.04-1.6-4.04-1.6-.55-1.39-1.34-1.76-1.34-1.76-1.1-.75.08-.74.08-.74 1.22.09 1.87 1.26 1.87 1.26 1.08 1.86 2.84 1.33 3.53 1.02.11-.78.42-1.33.76-1.63-2.67-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.16 0 0 1.01-.32 3.3 1.23A11.45 11.45 0 0112 5.8a11.42 11.42 0 012.99.4c2.29-1.55 3.3-1.23 3.3-1.23.66 1.64.24 2.86.12 3.16.77.84 1.24 1.91 1.24 3.22 0 4.61-2.8 5.63-5.48 5.93.43.37.81 1.1.81 2.22v3.29c0 .32.22.7.83.58A12 12 0 0012 0z" />
+          </svg>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/maheep-saluja/"
+          target="_blank"
+          className="hover:text-blue-700 transition"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className="w-5 h-5"
+          >
+            <path d="M4.98 3.5C4.98 4.88 3.86 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 24h5V7H0v17zm7.5 0h5V14.5c0-2.19 3-2.37 3 0V24h5V13c0-5.17-6-4.97-8 0V7H7.5v17z" />
+          </svg>
+        </a>
+      </div>
         { progress === 5 ? null : 
         <ProgressBar progress={progress} />}
         <Routes>
